75442ca8a28a6806dc3520d7b6dec2c8be707e7f
benimbriaco1 <benimbriaco06@gmail.com>
2025-02-14 09:46:12 -0500

    Calendar added

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-
 # Tkinter used for GUI
-from tkinter import *
+from tkinter import Tk
+import tkinter as tk
+# For calendar
+from tkcalendar import Calendar 
+
+def show_date():
+    selected_date = cal.get_date()
+    label.config(text=f"Selected Date: {selected_date}")
 
 # Create the window, giving it a title, and its dimensions
-r = Tk()
-r.title("To-Do List")
-r.geometry('400x400')
+root = Tk()
+root.title("To-Do List")
+root.geometry('400x400')
+# Color
+root.configure(bg="LightSteelBlue2")
 
+# Adding a calendar
+cal = Calendar(root, selectmode="day", date_pattern="m/d/yy", 
+background="white", foreground = "green")
+cal.pack(pady=20)
+
+# Label for date
+label = tk.Label(root, text="Selected Date: ")
+label.pack(pady=10)
+
+# Button for the show date function
+button = tk.Button(root, text="Get Date", command=show_date)
+button.pack()
 
 # Executing the GUI
-r.mainloop()
+root.mainloop()
