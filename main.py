7bc4eab07e64ee685cbe47385231459714cbb7df
benimbriaco1 <benimbriaco06@gmail.com>
2025-02-13 21:35:35 -0500

    GUI created

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# Tkinter used for GUI
+from tkinter import *
+
+# Create the window, giving it a title, and its dimensions
+r = Tk()
+r.title("To-Do List")
+r.geometry('400x400')
+
+
+# Executing the GUI
+r.mainloop()
