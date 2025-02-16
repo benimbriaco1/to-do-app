912ca680efe74786535a22d91f77a3a1a716e46c
benimbriaco1 <benimbriaco06@gmail.com>
2025-02-16 18:13:06 -0500

    pain

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from tkcalendar import Calendar
 
 # Global variable for seeing if there is a listbox already present
 listbox_present = False;
+
+# Create the window, giving it a title, and its dimensions
+root = Tk()
+root.title("To-Do List")
+root.geometry('400x600')
+# Color
+root.configure(bg="LightSteelBlue2")
+
 # Function for displaying the date
 def show_date(event):
     selected_date = cal.get_date()
@@ -19,20 +27,23 @@ def show_list():
     if not listbox_present:
         # Then create it
         lb = tk.Listbox(root)
+        # Change boolean
         listbox_present = True;
         lb.configure(background="lightblue", width=43, height = 15)
         lb.pack(pady=10)
 
 # Function for adding entry
 def add_entry():
-    print("hi")
+    task_label.pack(pady=10)
+    task_entry.pack(pady=10)
 
-# Create the window, giving it a title, and its dimensions
-root = Tk()
-root.title("To-Do List")
-root.geometry('400x600')
-# Color
-root.configure(bg="LightSteelBlue2")
+    task = task_var.get().strip()
+    if task:
+        print(f"added {task}")
+        task_var.set("")
+
+# Variable for user input
+task_var=tk.StringVar()
 
 # Adding a calendar
 cal = Calendar(root, selectmode="day", date_pattern="mm/dd/yy", 
@@ -46,6 +57,11 @@ label.pack(pady=10)
 
 # Binding the show_date function
 cal.bind("<<CalendarSelected>>", show_date)
+
+# Label for entry
+task_label = tk.Label(root, text="add task")
+# Creating an entry
+task_entry = tk.Entry(root, textvariable=task_var, font=('calibre',12,'bold'))
 
 # Adding a button for adding a task
 add = tk.Button(root, text="+", font="Arial 15", command=add_entry)
